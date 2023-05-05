de7f00ebbd38bb2eada7070eaba04131e1e327cc
Nicolas de gheselle <nicolas@degheselle.fr>
2023-05-05 22:35:36 +0200

    Added base categories for account

--- src/routes/(app)/accounts/[id]/AccountDetails.svelte
+++ src/routes/(app)/accounts/[id]/AccountDetails.svelte
@@ -1,5 +1,19 @@
 <script>
     import {categories} from "@lib/store";
+
+    let accountCategories = [];
+
+    $: updateAccountCategories(account, $categories);
+
+    function updateAccountCategories(_account, _categories)
+    {
+        accountCategories = [];
+        for (let cat of _account.categories)
+        {
+            _categories.find(c => c.id == cat.id);
+        }
+    }
+
     export let account = null;
 </script>
 
@@ -13,7 +27,7 @@
         </h2>
         <span class="has-text-grey">{account.name}</span>
         <div class="progress">
-            {#each accountCategories as category}
+            {#each account.categories as category}
                 <div
                     class="progress-bar"
                     style="background-color: {category.color}; width: {(category.total * 100) / account.categoriesTotal()}%;"
