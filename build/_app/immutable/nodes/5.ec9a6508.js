ce69e9cacfcf995bc35196b033552199f76611aa
Nicolas de gheselle <nicolas@degheselle.fr>
2023-07-02 12:14:37 +0200

    Added build to main

---
+++ build/_app/immutable/nodes/5.ec9a6508.js
@@ -0,0 +1,9 @@
+import{S as u,i as h,s as p,k as d,q as c,l as f,m as _,r as l,h as a,b as m,E as x,C as r}from"../chunks/index.14546b91.js";function g(i){let e,n,t;return{c(){e=d("h1"),n=c("Projects page"),t=c(`\r
+\r
+Should projects be linked to an account ?\r
+\r
+- Because money should come from somewhere I guess ?`)},l(o){e=f(o,"H1",{});var s=_(e);n=l(s,"Projects page"),s.forEach(a),t=l(o,`\r
+\r
+Should projects be linked to an account ?\r
+\r
+- Because money should come from somewhere I guess ?`)},m(o,s){m(o,e,s),x(e,n),m(o,t,s)},p:r,i:r,o:r,d(o){o&&a(e),o&&a(t)}}}class y extends u{constructor(e){super(),h(this,e,null,g,p,{})}}export{y as component};
