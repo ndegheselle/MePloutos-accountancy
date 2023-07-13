91176e6e0a682fb998cdef3b366ec3393de139ab
Nicolas de gheselle <nicolas@degheselle.fr>
2023-07-13 21:19:13 +0200

    removed build folder from git

--- build/_app/immutable/chunks/Alert.8b0edff6.js
+++
@@ -1,1 +0,0 @@
-import{w as o}from"./index.69270f44.js";function r(s,n){e.set({message:s,type:n})}const e=o(null),t={show:r,error:s=>r(s,"is-danger"),warning:s=>r(s,"is-warning"),info:s=>r(s,"is-info"),success:s=>r(s,"is-success")};export{e as a,t as b};
